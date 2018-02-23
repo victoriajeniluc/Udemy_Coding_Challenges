75d2182a0da53049e48622c9084faf9467c8f09a
Victoria Luc <victoriajeniluc@gmail.com>
2018-02-23 17:07:32 -0600

    added conditions to the program

--- HOISTING/practice.js
+++ HOISTING/practice.js
@@ -7,12 +7,12 @@ $(function(){
 		var birthYearOfPerson = $form.find("input[name='Year']").val();
 
 		function calculateAge(nameOfPerson, birthYearOfPerson) {	
-			if (nameOfPerson.length > 0 && birthYearOfPerson > 1915 && birthYearOfPerson < 2018 && birthYearOfPerson != '') {
+			if (nameOfPerson.length > 0 && birthYearOfPerson > 1000 && birthYearOfPerson < 2018 && birthYearOfPerson != '') {
 			var age = 2018 - birthYearOfPerson;
 			return `${nameOfPerson} is going to be ${age} this year!`;
 			} else if (birthYearOfPerson > 2018) {
 				return 'dumb ass.... you are definitely not born in the future';
-			} else if (birthYearOfPerson < 1920 && birthYearOfPerson != '') {
+			} else if (birthYearOfPerson < 1000 && birthYearOfPerson != '') {
 				return 'you are most likely to be dead by now';
 			} else {
 				return 'Uh you did not input the right info.... Please read the instructions and try again';
